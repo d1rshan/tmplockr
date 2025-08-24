1e97833b8c7019cfdf33a806ad09268b9852153e
darshan <darshan.paccha@gmail.com>
2025-08-24 20:13:59 +0530

    feat: add user card ui with shadcn charts

--- app/(app)/dashboard/_components/user-card.tsx
+++ app/(app)/dashboard/_components/user-card.tsx
@@ -1,5 +1,40 @@
 "use client";
 
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
+// import { useUser } from "@/features/user/hooks/use-user";
+
+// export const UserCard = () => {
+//   const { data: user } = useUser();
+
+//   return (
+//     <Card>
+//       <CardHeader>
+//         <CardTitle>Usage Details</CardTitle>
+//         <CardDescription>FILES LIMIT: 50MB btw NOTES: 10</CardDescription>
+//       </CardHeader>
+//       <CardContent>
+//         {user && user.notesUsed} Notes Used!
+//         <br />
+//         {user && (user.storageUsed / (1024 * 1024)).toFixed(2)}MB Storage Used!
+//       </CardContent>
+//     </Card>
+//   );
+// };
+
+import {
+  Label,
+  PolarGrid,
+  PolarRadiusAxis,
+  RadialBar,
+  RadialBarChart,
+} from "recharts";
+
 import {
   Card,
   CardContent,
@@ -7,22 +42,108 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { ChartConfig, ChartContainer } from "@/components/ui/chart";
+
 import { useUser } from "@/features/user/hooks/use-user";
 
-export const UserCard = () => {
+const chartConfig = {
+  storage: { label: "Storage", color: "hsl(var(--chart-1))" },
+  notes: { label: "Notes", color: "hsl(var(--chart-2))" },
+} satisfies ChartConfig;
+
+export function UserCard() {
   const { data: user } = useUser();
 
+  if (!user) {
+    return null;
+  }
+
+  const chartData = [
+    {
+      key: "storage",
+      value: Number((user?.storageUsed / (1024 * 1024)).toFixed(2)),
+      max: Number(process.env.NEXT_PUBLIC_FILES_LIMIT),
+    },
+    {
+      key: "notes",
+      value: user?.notesUsed,
+      max: Number(process.env.NEXT_PUBLIC_NOTES_LIMIT),
+    },
+  ];
   return (
-    <Card>
-      <CardHeader>
+    <Card className="flex flex-col">
+      <CardHeader className="items-center pb-0">
         <CardTitle>Usage Details</CardTitle>
-        <CardDescription>FILES LIMIT: 50MB btw NOTES: 10</CardDescription>
+        <CardDescription>Current quota usage</CardDescription>
       </CardHeader>
-      <CardContent>
-        {user && user.notesUsed} Notes Used!
-        <br />
-        {user && (user.storageUsed / (1024 * 1024)).toFixed(2)}MB Storage Used!
+
+      <CardContent className="flex gap-6 justify-center pb-0">
+        {chartData.map((item) => {
+          const config = chartConfig[item.key as keyof typeof chartConfig];
+
+          return (
+            <ChartContainer
+              key={item.key}
+              config={chartConfig}
+              className="aspect-square max-h-[250px] flex-1"
+            >
+              <RadialBarChart
+                data={[item]}
+                startAngle={0}
+                endAngle={(item.value / item.max) * 360} // ✅ arc length depends on usage
+                innerRadius={80}
+                outerRadius={110}
+              >
+                <PolarGrid
+                  gridType="circle"
+                  radialLines={false}
+                  stroke="none"
+                  className="first:fill-muted last:fill-background"
+                  polarRadius={[86, 74]}
+                />
+                <RadialBar
+                  dataKey="value"
+                  cornerRadius={10}
+                  fill={config.color}
+                />
+                <PolarRadiusAxis tick={false} tickLine={false} axisLine={false}>
+                  <Label
+                    content={({ viewBox }) => {
+                      if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                        return (
+                          <text
+                            x={viewBox.cx}
+                            y={viewBox.cy}
+                            textAnchor="middle"
+                            dominantBaseline="middle"
+                          >
+                            <tspan
+                              x={viewBox.cx}
+                              y={viewBox.cy}
+                              className="fill-foreground text-2xl font-bold"
+                            >
+                              {item.value}
+                              {item.key === "storage" ? " MB" : ""}
+                            </tspan>
+                            <tspan
+                              x={viewBox.cx}
+                              y={(viewBox.cy || 0) + 20}
+                              className="fill-muted-foreground text-sm"
+                            >
+                              {/* {percentage.toFixed(0)}% of {item.max}{" "} */}
+                              {config.label}
+                            </tspan>
+                          </text>
+                        );
+                      }
+                    }}
+                  />
+                </PolarRadiusAxis>
+              </RadialBarChart>
+            </ChartContainer>
+          );
+        })}
       </CardContent>
     </Card>
   );
-};
+}
