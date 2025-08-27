d9986c31004bdf00488b417a916c456523120339
darshan <darshan.paccha@gmail.com>
2025-08-27 17:52:53 +0530

    feat: update recieve page to display the shared files and notes

--- app/(app)/recieve/page.tsx
+++ app/(app)/recieve/page.tsx
@@ -16,16 +16,21 @@ import {
   InputOTPGroup,
   InputOTPSlot,
 } from "@/components/ui/input-otp";
+import { File, Note } from "@/types";
 
 export default function RecievePage() {
   const [code, setCode] = useState<string>("");
+  const [files, setFiles] = useState<File[]>([]);
+  const [notes, setNotes] = useState<Note[]>([]);
 
   const handleSubmit = async () => {
     if (code.length != 4) {
       return;
     }
     const res = await axios.get(`/api/shares/${code}`);
-    console.log(res.data);
+
+    setFiles(res.data.files);
+    setNotes(res.data.notes);
   };
 
   return (
@@ -35,18 +40,30 @@ export default function RecievePage() {
           <CardTitle>Enter Recieve Code</CardTitle>
         </CardHeader>
         <CardContent className="flex flex-col gap-4 items-center">
-          <InputOTP
-            maxLength={4}
-            value={code}
-            onChange={(value: string) => setCode(value)}
-          >
-            <InputOTPGroup>
-              <InputOTPSlot index={0} />
-              <InputOTPSlot index={1} />
-              <InputOTPSlot index={2} />
-              <InputOTPSlot index={3} />
-            </InputOTPGroup>
-          </InputOTP>
+          {files.length == 0 && notes.length == 0 && (
+            <InputOTP
+              maxLength={4}
+              value={code}
+              onChange={(value: string) => setCode(value)}
+            >
+              <InputOTPGroup>
+                <InputOTPSlot index={0} />
+                <InputOTPSlot index={1} />
+                <InputOTPSlot index={2} />
+                <InputOTPSlot index={3} />
+              </InputOTPGroup>
+            </InputOTP>
+          )}
+          {(files.length > 0 || notes.length > 0) && (
+            <div>
+              {files.map((file) => (
+                <div key={file.id}>{file.name}</div>
+              ))}
+              {notes.map((note) => (
+                <div key={note.id}>{note.title}</div>
+              ))}
+            </div>
+          )}
         </CardContent>
         <CardFooter>
           <Button onClick={handleSubmit} disabled={code.length != 4}>
