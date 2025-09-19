4a1387e9ec96982b5cdcdeae23ef084c47d6f8ce
d1rshan <darshan.paccha@gmail.com>
2025-09-20 00:47:52 +0530

    feat: add animated modal for login form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ModalButton } from "@/components/left-slide-modal";
 import { ModeToggle } from "@/components/mode-toggle";
 import { Button } from "@/components/ui/button";
 
@@ -17,7 +18,7 @@ export const GridBackground = () => {
     <div
       className="-z-10 w-full h-screen fixed top-0 left-0 bg-background [--line-color:var(--color-neutral-800)]
               bg-[linear-gradient(0deg,transparent_24%,var(--line-color)_25%,var(--line-color)_26%,transparent_27%,transparent_74%,var(--line-color)_75%,var(--line-color)_76%,transparent_77%,transparent),linear-gradient(90deg,transparent_24%,var(--line-color)_25%,var(--line-color)_26%,transparent_27%,transparent_74%,var(--line-color)_75%,var(--line-color)_76%,transparent_77%,transparent)] 
-             bg-[size:55px_55px] mask-radial-from-50"
+             bg-[size:55px_55px] mask-radial-from-20"
     />
   );
 };
@@ -31,16 +32,34 @@ export const Hero = () => {
    ██║   ██║ ╚═╝ ██║██║     ███████╗╚██████╔╝╚██████╗██║  ██╗██║  ██║
    ╚═╝   ╚═╝     ╚═╝╚═╝     ╚══════╝ ╚═════╝  ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝
   `;
+
   return (
-    <div className="flex flex-col items-center justify-center">
+    <div className="flex flex-col items-center justify-center ">
       <pre className="[line-height:1] text-neutral-50">{hero}</pre>
       <div className="flex gap-x-2 ">
-        <Button>Send Files</Button>
-        <Button>Recieve Files</Button>
+        <ModalButton>Enter</ModalButton>
+        <ModalButton>Recieve</ModalButton>
       </div>
     </div>
   );
 };
+
+export const FancyButton = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <a href="#_" className="relative inline-block text-lg group font-mono">
+      <span className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight  transition-colors duration-300 ease-out border-2 rounded-lg group-hover:text-black">
+        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-background"></span>
+        <span className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-neutral-100 group-hover:-rotate-180 ease"></span>
+        <span className="relative">{children}</span>
+      </span>
+      <span
+        className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-background rounded-lg group-hover:mb-0 group-hover:mr-0"
+        data-rounded="rounded-lg"
+      ></span>
+    </a>
+  );
+};
+
 export const Navbar = () => {
   return (
     <nav className="z-10 sticky top-0 md:top-2 px-4 py-2 border bg-background-400/70 backdrop-blur-md  md:rounded-xl  flex justify-end items-center">
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,8 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-background border border-neutral-100 shadow-md",
+        default:
+          "font-mono bg-background border border-neutral-50 hover:bg-neutral-50 hover:text-background  shadow-md",
         // default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
