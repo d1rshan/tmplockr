d69ca7b673bc7d988bba5b45e67efeb09c091791
d1rshan <darshan.paccha@gmail.com>
2025-09-21 02:01:34 +0530

    feat: make navbar responsive

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,9 @@
+"use client";
+
+import { LogOut } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 const logo = `
 ████████╗███╗   ███╗██████╗ ██╗      ██████╗  ██████╗██╗  ██╗██████╗ 
@@ -11,17 +15,28 @@ const logo = `
   `;
 
 export const Navbar = () => {
+  const pathname = usePathname();
+
+  const isDashboard = pathname === `/dashboard`;
   return (
     <nav className="z-10 fixed top-0 left-0 w-full bg-[#ECEEF0]/65 dark:bg-[#1E1E24]/65 backdrop-blur-md flex items-center justify-between p-2 border-b-[0.5px]">
       <div className="flex-1"></div>
 
-      <div className="flex-1 flex justify-center">
-        <pre className="ascii-art text-[5px] select-none">{logo}</pre>
-      </div>
-
+      {isDashboard && (
+        <div className="flex-1 flex justify-center items-center">
+          <pre className="ascii-art text-[3px] sm:text-[5px] select-none">
+            {logo}
+          </pre>
+        </div>
+      )}
       <div className="flex-1 flex justify-end gap-2 items-center">
+        {isDashboard && (
+          <Button variant="outline">
+            <span className="hidden sm:block">SIGN OUT</span>
+            <LogOut className="sm:hidden" />
+          </Button>
+        )}
         <ModeToggle />
-        <Button variant="outline">SIGN OUT</Button>
       </div>
     </nav>
   );
