243fac4553cf9b29ae8ee4404f4ea70132e44ee4
d1rshan <darshan.paccha@gmail.com>
2025-10-17 19:07:44 +0530

    feat: add reusable auth hook

---
+++ src/features/auth/hooks/auth.ts
@@ -0,0 +1,24 @@
+import "server-only";
+
+import { cookies } from "next/headers";
+import { jwtVerify } from "jose";
+
+export async function auth() {
+  const token = (await cookies()).get("__lol")?.value;
+
+  // 2. If no token, return null
+  if (!token) {
+    return null;
+  }
+
+  // 3. Verify the token
+  try {
+    const secretKey = new TextEncoder().encode(process.env.JWT_SECRET);
+    const { payload } = await jwtVerify(token, secretKey);
+    return payload;
+  } catch (error) {
+    // 4. If verification fails, return null
+    console.error("JWT Verification failed:", error);
+    return null;
+  }
+}
